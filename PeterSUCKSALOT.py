a88d727ff145353312da90c46fe11247c0ce6e89
KyleSmith121 <kyle@kcorp.info>
2015-04-10 20:33:11 -0700

    Added PETERSUCKSALOT.py
    Added a file to count items in list and return a "Hello World"

---
+++ PeterSUCKSALOT.py
@@ -0,0 +1,12 @@
+__author__ = 'Kyle'
+
+number = 0
+
+myList = ["Kyle", "Smith", 9255488177, None]
+newList = []
+
+
+for each in myList:
+        if each is not None:
+            print("Hello World")
+            newList.append(each)
